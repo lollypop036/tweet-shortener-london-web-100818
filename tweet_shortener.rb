225e180e054b1c7abe3573543303a6001ef3585e
Lola Onanuga <slonanuga@gmail.com>
2018-10-02 20:34:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 def dictionary
   
-  hash = {hello => "hi", to =>"2", two =>"2", too => "2", "for" => "4", four: "4", be: "b", you: "u", at: "@", "and" => "&"}
+  hash = {"hello" => "hi", "to" =>"2", "two" =>"2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
   
   hash
 end
