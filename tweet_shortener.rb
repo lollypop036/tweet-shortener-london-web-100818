2c06c3bf246b29c795befa317a047efbf3fbf68e
Lola Onanuga <slonanuga@gmail.com>
2018-10-02 20:31:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 def dictionary
   
-  hash = {hello: "hi", to: "2", two: "2", too: "2", "for" => "4", four: "4", be: "b", you: "u", at: "@", "and" => "&"}
+  hash = {hello => "hi", to =>"2", two =>"2", too => "2", "for" => "4", four: "4", be: "b", you: "u", at: "@", "and" => "&"}
   
   hash
 end
@@ -10,8 +10,10 @@ def word_substituter(tweet)
   
   tweet = tweet.split(" ")
   tweet.map do |x|
-  if  dictionary.keys.include?(x.to_sym)
-    x = dictionary.values_at(x.to_sym)
+  if  dictionary.keys.include?(x)
+    x = dictionary.values_at(x)
+    
+    elsif dic
 end
 
 end
